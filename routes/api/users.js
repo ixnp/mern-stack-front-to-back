43fb00bf6cf114033699b4d19e045c6b62adf645
ixnp <enprocopios@gmail.com>
2018-08-20 22:00:11 -0700

    loging route no without token working

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,5 +45,31 @@ router.post('/register', (req, res) => {
     });
 });
 
+//@route  GET api/post/login
+//@desc   login user
+//@access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  //find user by email
+  User.findOne({email})
+    .then(user => {
+    //check for user
+      if(!user){
+        return res.status(404).json({email:'User not found'});
+      }
+
+      //check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            res.json({msg: 'Sucess'});
+          } else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        });
+    });
+});
 
 module.exports = router;
