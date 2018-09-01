1c13cfabc59f8fc216a8039a37ee6cab26b6411f
ixnp <enprocopios@gmail.com>
2018-09-01 14:48:52 -0700

    profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+//Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 //Load Profile Model
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -32,6 +35,15 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 //@desc   Create or edit users profile
 //@access Private
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  
+  const { errors, isValid} = validateProfileInput(req.body);
+
+  // Check Validation
+  if(!isValid){
+    //Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+  
   // Get fields
   const profileFields = {};
   profileFields.user = req.user.id;
@@ -66,9 +78,22 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
           { user: req.user.id}, 
           {$set: profileFields}, 
           {new:true}
-        );
+        )
+          .then(profile => res.json(profile));
       }else {
       //create
+
+      //check if handle exists
+        Profile.findOne({ handle: profileFields.handle}).then(profile => {
+          if(profile) {
+            errors.handle = 'That handle already exists';
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+
+        });
       }
     });
 });
