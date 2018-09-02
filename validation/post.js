4d3eb7149ee68ca717f6aa98d8b4595acf7202bc
ixnp <enprocopios@gmail.com>
2018-09-02 15:30:55 -0700

    creating post routes and validation

---
+++ validation/post.js
@@ -0,0 +1,24 @@
+'use strict'; 
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data){
+  let errors = {};
+  //Turns data into an empty string if data.name does not have a value 
+ 
+  data.text = !isEmpty(data.text) ? data.text : '';
+   
+    
+ if(!Validator.isLength(data.text, {min: 10, max: 300})) {
+   errors.text = 'Post much be between 10 and 300 characters';
+ }
+
+  if(Validator.isEmpty(data.text)){
+    errors.text = 'Text field is required';
+  }
+
+  return {
+    errors, 
+    isValid: isEmpty(errors)
+  };
+};
