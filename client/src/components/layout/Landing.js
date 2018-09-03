ba46f6f9c319ae5b1a5c60ce777a0196df7914f0
ixnp <enprocopios@gmail.com>
2018-09-03 13:16:32 -0700

    built out react router

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Landing extends Component {
   render() {
@@ -13,9 +14,8 @@ class Landing extends Component {
             </h1>
             <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
             <hr />
-            {/* need to change to link tab instead of href */}
-            <a href="register.html" class="btn btn-lg btn-info mr-2">Sign Up</a>
-            <a href="login.html" class="btn btn-lg btn-light">Login</a>
+            <Link to="/register" class="btn btn-lg btn-info mr-2">Sign Up</Link>
+            <Link to="/login" class="btn btn-lg btn-light">Login</Link>
           </div>
         </div>
       </div>
