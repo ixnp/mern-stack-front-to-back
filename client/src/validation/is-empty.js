25c7bf40555f79c472c0484ee3f5135382bed84a
ixnp <enprocopios@gmail.com>
2018-09-04 16:03:15 -0700

    started auth reducer and register action

---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,9 @@
+'use strict';
+//the validators npm package we are using only checks for a str this handles for everything else 
+const isEmpty = value => 
+  value === undefined ||
+  value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0);
+  
+export default isEmpty;
